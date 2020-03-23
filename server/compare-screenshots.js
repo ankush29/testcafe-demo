060bc1dc9ea7c4f8bef6d4983309b00119047218
Ankush Saini <anksaini1@publicisgroupe.net>
2020-03-23 21:20:47 +0530

    change image comparision library

--- server/compare-screenshots.js
+++ server/compare-screenshots.js
@@ -1,31 +1,14 @@
-import Rembrandt from 'rembrandt'
-import Colors from 'colors'
-
+import looksSame from 'looks-same'
 
 export default function (screenshotPath, modelPath) {
-	const rembrandt = new Rembrandt({
-	  imageA: screenshotPath,
-	  imageB: modelPath,
-
-	  thresholdType: Rembrandt.THRESHOLD_PERCENT,
-	  maxThreshold: 0.01,
-	  maxDelta: 20,
-	  maxOffset: 0,
-	  renderComposition: true, // Should Rembrandt render a composition image?
-	  compositionMaskColor: Rembrandt.Color.RED // Color of unmatched pixels
-	})
-
-	// Run the comparison
-    return rembrandt.compare()
-	  .then(function (result) {
-	  	const difference = 'Difference: ' + (result.threshold * 100).toFixed(2) + '%';
-
-	    if (result.passed) {
-	    	console.log(" Images are equal".gray);
-	    } else {
-	    	console.log("Passed: false".bold.green);
-	    	console.log(difference.bold.green);
-	    	throw new Error("Images are different".bold.red);
-	    }
-	  })
+	looksSame(screenshotPath, modelPath, function(error, {equal}) {
+		// equal will be true, if images looks the same
+		console.log('error---',error,'equal-----',equal)
+		if(equal) {
+			console.log(" Images are equal");
+		} else {
+			console.log("Passed: false");
+			throw new Error("Images are different");
+		}
+	});
 }
