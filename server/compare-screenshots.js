ef71caa0e7af619c275303545def6c187a502798
Ankush Saini <anksaini1@publicisgroupe.net>
2020-03-23 20:28:03 +0530

    add screenshot compare

--- server/compare-screenshots.js
+++ server/compare-screenshots.js
@@ -6,22 +6,22 @@ export default function (screenshotPath, modelPath) {
 	const rembrandt = new Rembrandt({
 	  imageA: screenshotPath,
 	  imageB: modelPath,
-	 
+
 	  thresholdType: Rembrandt.THRESHOLD_PERCENT,
 	  maxThreshold: 0.01,
 	  maxDelta: 20,
-	  maxOffset: 0,	 
-	  renderComposition: true, // Should Rembrandt render a composition image? 
-	  compositionMaskColor: Rembrandt.Color.RED // Color of unmatched pixels 
+	  maxOffset: 0,
+	  renderComposition: true, // Should Rembrandt render a composition image?
+	  compositionMaskColor: Rembrandt.Color.RED // Color of unmatched pixels
 	})
-	 
-	// Run the comparison 
+
+	// Run the comparison
     return rembrandt.compare()
 	  .then(function (result) {
-	  	var difference = 'Difference: ' + (result.threshold * 100).toFixed(2) + '%';
+	  	const difference = 'Difference: ' + (result.threshold * 100).toFixed(2) + '%';
 
 	    if (result.passed) {
-	    	console.log(" Images are equal".gray);	    	
+	    	console.log(" Images are equal".gray);
 	    } else {
 	    	console.log("Passed: false".bold.green);
 	    	console.log(difference.bold.green);
